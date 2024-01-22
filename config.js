eca56e1353e07649d7fcf5759b790f7997324c04
Dungauto <144578719+Dungauto@users.noreply.github.com>
2024-01-22 11:43:59 +0700

    Add files via upload
    Card Birthday

---
+++ config.js
@@ -0,0 +1,10 @@
+const nameGirl = 'Bé NHG';
+const giftUrl = 'https://www.facebook.com/leducdung.2001';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Thiệp sinh nhật';
+const contentCard = 'Từ khi em xuất hiện cuộc sống của anh bắt đầu tô màu, bánh xe vận mệnh xoay chuyển. Anh vui lắm, tuổi mới chúc em luôn vui vẻ, hạnh phúc, cười thật nhiều nhá.';
+
+
+const giftImage = 'hot-girl.png';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
